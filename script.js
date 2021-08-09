989be9f9cf8d2b8e551942ca68e082d8ad093e52
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-08-09 11:24:47 -0500

    Add index.html file, and javascript file with objects array and function add

---
+++ script.js
@@ -0,0 +1,18 @@
+let books =[{
+  title: "",
+  author:"",
+}];
+
+const titlePlace = document.getElementById("title");
+const authorPlace = document.getElementById("author");
+const addButton = document.getElementById("add-button");
+
+
+function add() {
+  let bookTitle = document.createElement("h2");
+  let bookAuthor = document.createElement("h3");
+  console.log("hi");
+  
+}
+
+addButton.addEventListener("click", add);
