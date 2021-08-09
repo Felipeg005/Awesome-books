493059e9ca72f8bd8ad6978712931edc051fd9ca
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-08-09 15:35:09 -0500

    add objects to array and create remove button

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-let books =[{
-  title: "",
-  author:"",
-}];
+let books =[];
 
-const titlePlace = document.getElementById("title");
-const authorPlace = document.getElementById("author");
-const addButton = document.getElementById("add-button");
+const titlePlace = document.getElementById('title');
+const authorPlace = document.getElementById('author');
+const addButton = document.getElementById('add-button');
 const form = document.getElementById('form');
 
 function add(event) {
   event.preventDefault();
   let bookAuthor = document.createElement("h3");
   let title = document.createElement("h2");
+  let removeButton = document.createElement('button');
   title.innerText = titlePlace.value;
   bookAuthor.innerHTML = authorPlace.value;
+  removeButton.innerHTML = 'Remove Book';
   document.getElementById('books').appendChild(title);
   document.getElementById('books').appendChild(bookAuthor);
+  document.getElementById('books').appendChild(removeButton);
+  
+
+  let newBook = {
+    title: titlePlace.value, 
+    author: authorPlace.value,
+  }
+
+  books.push(newBook);
+
+  console.log(books);
+  console.log(authorPlace.value);
+  console.log(titlePlace.value);
+
   form.reset();
+}
+
+function removeBook() {
+
 }
 
 form.addEventListener('submit', add);
