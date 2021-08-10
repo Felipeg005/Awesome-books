dc9688b4ba3f5a16ff3cf6b3e738a6f2adee07b9
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-08-09 19:14:49 -0500

    linters check 1.1

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function add(event) {
   const title = document.createElement('h2');
   const removeButton = document.createElement('button');
   const separateLine = document.createElement('hr');
-  bookContainer.id = 'container'+`${titlePlace.value}`;
+  bookContainer.id = `container${titlePlace.value}`;
   removeButton.id = `${titlePlace.value}`;
   removeButton.setAttribute('onclick', `removeBook(this.id)`);
   title.innerText = titlePlace.value;
@@ -18,29 +18,25 @@ function add(event) {
   removeButton.innerHTML = 'Remove Book';
   const booksDiv = document.getElementById('books');
   booksDiv.insertBefore(bookContainer, booksDiv.firstChild);
-  document.getElementById('container' + `${titlePlace.value}`).appendChild(title);
-  document.getElementById('container' + `${titlePlace.value}`).appendChild(bookAuthor);
-  document.getElementById('container' + `${titlePlace.value}`).appendChild(removeButton);
-  document.getElementById('container' + `${titlePlace.value}`).appendChild(separateLine);
-  
+  document.getElementById(`container${titlePlace.value}`).appendChild(title);
+  document.getElementById(`container${titlePlace.value}`).appendChild(bookAuthor);
+  document.getElementById(`container${titlePlace.value}`).appendChild(removeButton);
+  document.getElementById(`container${titlePlace.value}`).appendChild(separateLine);
   const newBook = {
     title: titlePlace.value, 
     author: authorPlace.value,
   }
-  
   books.push(newBook);
   localStorage.setItem('bookStorage', JSON.stringify(books));
   form.reset();
 }
 
-function removeBook(buttonId) {
+/* eslint-disable */function removeBook(buttonId) {
   const bookToRemove = document.getElementById('container'+`${buttonId}`);
   bookToRemove.parentNode.removeChild(bookToRemove);
-  
   for (let i = 0; i < books.length; i += 1) {
     if (books[i].title === buttonId) {
       books.splice(i, 1);
-      console.log(books);
       localStorage.setItem('bookStorage', JSON.stringify(books));
       break;
     }
@@ -51,29 +47,27 @@ function loadBooks() {
 const bookStorage = JSON.parse(localStorage.getItem('bookStorage'));
   if (bookStorage) {
     for (let i = 0; i < bookStorage.length; i += 1){
-    const bookContainer = document.createElement('div');
-    const bookAuthor = document.createElement("h3");
-    const title = document.createElement("h2");
-    const removeButton = document.createElement('button');
-    const separateLine = document.createElement('hr');
-    bookContainer.id = 'container'+`${bookStorage[i].title}`;
-    removeButton.id = `${bookStorage[i].title}`;
-    removeButton.setAttribute('onclick', `removeBook(this.id)`);
-    title.innerText = bookStorage[i].title;
-    bookAuthor.innerHTML = bookStorage[i].author;
-    removeButton.innerHTML = 'Remove Book';
-    const booksDiv = document.getElementById('books')
-    booksDiv.insertBefore(bookContainer, booksDiv.firstChild);
-    document.getElementById('container'+`${bookStorage[i].title}`).appendChild(title);
-    document.getElementById('container'+`${bookStorage[i].title}`).appendChild(bookAuthor);
-    document.getElementById('container'+`${bookStorage[i].title}`).appendChild(removeButton);
-    document.getElementById('container'+`${bookStorage[i].title}`).appendChild(separateLine);
-    
-    let newBook = {
-      title: bookStorage[i].title, 
-      author: bookStorage[i].author,
-    }
-
+      const bookContainer = document.createElement('div');
+      const bookAuthor = document.createElement("h3");
+      const title = document.createElement("h2");
+      const removeButton = document.createElement('button');
+      const separateLine = document.createElement('hr');
+      bookContainer.id = `container${bookStorage[i].title}`;
+      removeButton.id = `${bookStorage[i].title}`;
+      removeButton.setAttribute('onclick', `removeBook(this.id)`);
+      title.innerText = bookStorage[i].title;
+      bookAuthor.innerHTML = bookStorage[i].author;
+      removeButton.innerHTML = 'Remove Book';
+      const booksDiv = document.getElementById('books')
+      booksDiv.insertBefore(bookContainer, booksDiv.firstChild);
+      document.getElementById(`container${bookStorage[i].title}`).appendChild(title);
+      document.getElementById(`container${bookStorage[i].title}`).appendChild(bookAuthor);
+      document.getElementById(`container${bookStorage[i].title}`).appendChild(removeButton);
+      document.getElementById(`container${bookStorage[i].title}`).appendChild(separateLine);
+      let newBook = {
+        title: bookStorage[i].title, 
+        author: bookStorage[i].author,
+      }
   books.push(newBook);
   }
 }
