a61adc6746f2e0be244e09f56a140f75d0fe053f
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-08-13 10:54:48 -0500

    luxon install and test

--- luxon.js
+++
@@ -1,7 +0,0 @@
-import { DateTime } from "luxon";
-const now = DateTime.now();
-const timeDiv = document.querySelector('.time')
-dt = DateTime.fromObject({day: 22, hour: 12 }, { zone: 'America/Los_Angeles', numberingSystem: 'beng'});
-DateTime.fromISO("2017-05-15T08:30:00");
-DateTime.now().toString();
-timeDiv.innerHTML = dt.toLocaleString(DateTime.DATETIME_MED);
--- script.js
+++ script.js
@@ -119,6 +119,14 @@ function removeBook(buttonId) {
 }
 removeBook();
 /* eslint max-classes-per-file: ["error", 2] */
+import { DateTime } from "https://moment.github.io/luxon/global/luxon.js";
+const time = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+const timeContainer = document.querySelector('#dateTime');
+const date = document.createElement('p');
+date.innerHTML = time;
+date.classList.add('float-right', 'me-5', 'my-3');
+timeContainer.appendChild(date);
+
 document.addEventListener('DOMContentLoaded', BooksArray.Get);
 form.addEventListener('submit', (...e) => {
   saveBook.add(...e);
