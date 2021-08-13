9d4c1d805276c71f41f4d239bc41c6e859da65b2
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-08-13 16:49:09 -0500

    linters and live demo version checks

--- script.js
+++ script.js
@@ -130,20 +130,18 @@ function removeBook(buttonId) {
 removeBook();
 /* eslint max-classes-per-file: ["error", 2] */
 function show(sectionId) {
-  if(sectionId) {
-    const sectionArr = ['list-section', 'form-section', 'contact-section'];
-    let sectionToShow = document.getElementById(`${sectionId}`);
-    if(`${sectionToShow.className}`==='') {
-      return;
-      } else {
-      for (let i = 0; i < sectionArr.length; i += 1) {
-        sectionToShow = document.getElementById(`${sectionArr[i]}`);
-        if (`${sectionToShow.className}`=== '') {
+  const sectionArr = ['list-section', 'form-section', 'contact-section'];
+  let sectionToShow = document.getElementById(`${sectionId}`);
+  if (`${sectionToShow.className}` === '') {
+    sectionId = '';
+  } else {
+    for (let i = 0; i < sectionArr.length; i += 1) {
+      sectionToShow = document.getElementById(`${sectionArr[i]}`);
+      if (`${sectionToShow.className}` === '') {
         sectionToShow.classList.toggle('d-none');
-        } else if((`${sectionToShow.className}`=== 'd-none') && (sectionArr[i]===sectionId)) {
-          sectionToShow = document.getElementById(`${sectionId}`);
-          sectionToShow.classList.toggle('d-none');
-        }
+      } else if ((`${sectionToShow.className}` === 'd-none') && (sectionArr[i] === sectionId)) {
+        sectionToShow = document.getElementById(`${sectionId}`);
+        sectionToShow.classList.toggle('d-none');
       }
     }
   }
